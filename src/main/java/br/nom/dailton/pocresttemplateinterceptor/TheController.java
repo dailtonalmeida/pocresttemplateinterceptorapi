e8b8fa100f53cf649a08ef7716824c4b761067d1
Dailton Santana de Almeida <dailton.almeida@gft.com>
2025-05-23 13:02:48 -0300

    tempo de expiracao vindo do servidor / 401 vindo da API

--- src/main/java/br/nom/dailton/pocresttemplateinterceptor/TheController.java
+++ src/main/java/br/nom/dailton/pocresttemplateinterceptor/TheController.java
@@ -3,12 +3,16 @@ package br.nom.dailton.pocresttemplateinterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.util.Map;
 
 @RestController
@@ -17,9 +21,17 @@ public class TheController {
     private static final Logger log = LoggerFactory.getLogger(TheController.class);
 
     @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, Object> hello(@RequestHeader(value = HttpHeaders.AUTHORIZATION) String auth) {
-        log.atInfo().log("RECEIVED AUTH TOKEN: {}", auth);
-        return Map.of("message", "Hello, I'm THE API!!!");
+    public ResponseEntity<Map<String, Object>> hello(@RequestHeader(value = HttpHeaders.AUTHORIZATION) String auth,
+                                     @RequestHeader(value = "X-EXPIRES-AT", required = false) Instant expiresAt) {
+        log.atInfo().log("RECEIVED AUTH TOKEN: {} EXPIRES-AT:{}", auth, expiresAt);
+
+        if (expiresAt == null || Instant.now(Clock.systemUTC()).isAfter(expiresAt)) {
+            log.atInfo().log("API WILL PRODUCE INFORMATION OF UNAUTHORIZED");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        log.atInfo().log("API WILL PRODUCE INFORMATION OF OK");
+        return ResponseEntity.ok(Map.of("message", "Hello, I'm THE API!!!"));
     }
 
 }
